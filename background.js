9db9554aec480f79f62676cde6ac514d59bf859b
Jacob Birkett <spikespaz@outlook.com>
2018-02-19 02:21:30 -0700

    Add custom theming using semi-transparent black overlays

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ browser.windows.onCreated.addListener(themeWindow);
 browser.windows.getAll().then(wins => wins.forEach(themeWindow));
 
 function themeWindow(window) {
-  // Check if the window is in private browsing
+  // Check if the window is in private browsing and use the full dark theme
   if (window.incognito) {
     browser.theme.update(window.id, {
       images: {
@@ -14,12 +14,22 @@ function themeWindow(window) {
         accentcolor: "black",
         textcolor: "white",
         toolbar: "#333",
-        toolbar_text: "white"
+        toolbar_text: "#fff"
       }
     });
-  }
-  // Reset to the default theme otherwise
-  else {
-    browser.theme.reset(window.id);
+  } else { // Not incognito, use adaptive theme
+    browser.theme.update(window.id, {
+      images: {
+        headerURL: "",
+      },
+      colors: {
+        accentcolor: "-moz-win-accentcolor",
+        textcolor: "-moz-win-accentcolortext",
+        toolbar: "#00000040", // 25% darker than accent
+        toolbar_text: "-moz-win-accentcolortext",
+        toolbar_field: "#0000007f", // 50% darker than accent
+        toolbar_field_text: "-moz-win-accentcolortext"
+      }
+    });
   }
 }
