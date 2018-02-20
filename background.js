278be28cbd0d2c4d5d5641135567f1b052ed9491
Jacob Birkett <spikespaz@outlook.com>
2018-02-20 05:39:21 -0700

    Add switch to control unfocused mode

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
-// Set to true to fade the color scheme instead of reset it when unfocused
-const fade_unfocused = true;
+// const unfocused_mode = "tabs"; // Make tabs white
+// const unfocused_mode = "title"; // Make background white and tabs accent color
+// const unfocused_mode = "both"; // Set accent color to white
+const unfocused_mode = "fade"; // Fade the tabs and URL bar
+// const unfocused_mode = "none"; // Anything else: only fade text
+
+function setTheme(window, colors) {
+    browser.theme.update(window.id, {
+        images: { headerURL: "" },
+        colors: colors
+    })
+}
 
 // Check if the platform is Windows, and if it can use the accent colors
 browser.runtime.getPlatformInfo().then(info => {
@@ -25,41 +35,44 @@ browser.runtime.getPlatformInfo().then(info => {
     function themeWindow(window) {
         if (!window.focused) {
             // Check if the window isn't in focus, reset it
-            if (fade_unfocused) {
-                browser.theme.update(window.id, {
-                    images: { headerURL: "" },
-                    colors: {
+            switch (unfocused_mode) {
+                case "fade": setTheme(window, {
                         accentcolor: accentcolor,
                         textcolor: "#fff",
                         toolbar: "#ffffff40", // 25% lighter than accent
                         toolbar_text: "#fff",
                         toolbar_field: "#ffffff40", // 50% lighter than accent
                         toolbar_field_text: "#fff"
-                    }
-                });
-            } else browser.theme.reset(window.id);
+                    });
+                    break;
+                case "both": setTheme(window, {
+                            accentcolor: "#fff",
+                            textcolor: "#000",
+                            toolbar: "#0000000f", // 10% darker than white
+                            toolbar_text: "#000",
+                            toolbar_field: "#0000000f", // 20% darker than white
+                            toolbar_field_text: "#000"
+                        });
+                    break;
+                default:
+                    break;
+            }
         } else if (window.incognito) {
             // Check if the window is in private browsing and use the full dark theme
-            browser.theme.update(window.id, {
-                images: { headerURL: "" },
-                colors: {
-                    accentcolor: "#222", // A little lighter than the default black
-                    textcolor: "#fff",
-                    toolbar: "#333",
-                    toolbar_text: "#fff"
-                }
+            setTheme(window, {
+                accentcolor: "#222", // A little lighter than the default black
+                textcolor: "#fff",
+                toolbar: "#333",
+                toolbar_text: "#fff"
             });
         } else { // Not incognito, use adaptive theme
-            browser.theme.update(window.id, {
-                images: { headerURL: "" },
-                colors: {
-                    accentcolor: accentcolor,
-                    textcolor: "#fff",
-                    toolbar: "#00000040", // 25% darker than accent
-                    toolbar_text: "#fff",
-                    toolbar_field: "#00000040", // 50% darker than accent
-                    toolbar_field_text: "#fff"
-                }
+            setTheme(window, {
+                accentcolor: accentcolor,
+                textcolor: "#fff",
+                toolbar: "#00000040", // 25% darker than accent
+                toolbar_text: "#fff",
+                toolbar_field: "#00000040", // 50% darker than accent
+                toolbar_field_text: "#fff"
             });
         }
     }
