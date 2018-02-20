f3c53eb7e70fca05ffd9bbd5e119d6d452dd9bd2
Jacob Birkett <spikespaz@outlook.com>
2018-02-19 19:56:14 -0700

    Add fade unfocused windows option instead of reset

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+// Set to true to fade the color scheme instead of reset it when unfocused
+const fade_unfocused = true;
+
 // Check if the platform is Windows, and if it can use the accent colors
 browser.runtime.getPlatformInfo().then(info => {
     let accentcolor, accentcolortext
@@ -22,13 +25,23 @@ browser.runtime.getPlatformInfo().then(info => {
     function themeWindow(window) {
         if (!window.focused) {
             // Check if the window isn't in focus, reset it
-            browser.theme.reset(window.id);
+            if (fade_unfocused) {
+                browser.theme.update(window.id, {
+                    images: { headerURL: "" },
+                    colors: {
+                        accentcolor: accentcolor,
+                        textcolor: "#fff",
+                        toolbar: "#ffffff40", // 25% lighter than accent
+                        toolbar_text: "#fff",
+                        toolbar_field: "#ffffff40", // 50% lighter than accent
+                        toolbar_field_text: "#fff"
+                    }
+                });
+            } else browser.theme.reset(window.id);
         } else if (window.incognito) {
             // Check if the window is in private browsing and use the full dark theme
             browser.theme.update(window.id, {
-                images: {
-                    headerURL: "",
-                },
+                images: { headerURL: "" },
                 colors: {
                     accentcolor: "#222", // A little lighter than the default black
                     textcolor: "#fff",
@@ -38,9 +51,7 @@ browser.runtime.getPlatformInfo().then(info => {
             });
         } else { // Not incognito, use adaptive theme
             browser.theme.update(window.id, {
-                images: {
-                    headerURL: "",
-                },
+                images: { headerURL: "" },
                 colors: {
                     accentcolor: accentcolor,
                     textcolor: "#fff",
