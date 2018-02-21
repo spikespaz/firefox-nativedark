7ec7f37f1fbf5092bc35dfc2eea5a7eaf9dead24
Jacob Birkett <spikespaz@outlook.com>
2018-02-21 11:43:46 -0700

    Make options page load local storage when opened

--- options.js
+++ options.js
@@ -12,3 +12,11 @@ optionsForm.addEventListener("change", () => {
 });
 
 console.log("Calling from options.js, loaded!");
+
+browser.storage.local.get().then(optionsData => {
+    if (optionsData.accentColor === "-moz-win-accentcolor")
+        document.getElementById("enable-adaptive").checked = true;
+    else document.getElementById("accent-color").value = optionsData.accentColor;
+
+    document.getElementById("unfocused-theme").value = optionsData.unfocusedTheme;
+});
