a1e13ead740da395e84afd509f533d6e2e75399b
Jacob Birkett <spikespaz@outlook.com>
2018-02-22 11:56:32 -0700

    Change the options script to constantly update and add color picker support

--- options.js
+++ options.js
@@ -1,6 +1,14 @@
 const optionsForm = document.getElementById("options-form");
+const optionsSubmit = document.getElementById("options-sumbit");
 
-optionsForm.addEventListener("change", () => {
+const accentColor = document.getElementById("accent-color");
+const accentColorText = document.getElementById("accent-color-text");
+
+const enableAdaptive = document.getElementById("enable-adaptive");
+
+// Update the values in the local storage when the form is submitted (rebound to change event)
+optionsForm.addEventListener("submit", event => {
+    console.log("Triggered submit");
     let formData = new FormData(optionsForm);
     let formJSON = {};
 
@@ -9,14 +17,35 @@ optionsForm.addEventListener("change", () => {
     }
 
     browser.storage.local.set(formJSON);
+    console.log(formJSON);
+    event.preventDefault();
+});
+
+// Bind all changes made to the form to automatically submit, uses fake button
+optionsForm.addEventListener("input", () => optionsSubmit.click());
+
+accentColor.addEventListener("input", () => accentColorText.value = accentColor.value.toUpperCase());
+accentColorText.addEventListener("input", () => accentColor.value = accentColorText.value);
+
+// Toggle disabled for color poicker when enable adaptive is changed
+enableAdaptive.addEventListener("change", () => {
+    accentColor.disabled = enableAdaptive.checked;
+    accentColorText.disabled = enableAdaptive.checked;
 });
 
 console.log("Calling from options.js, loaded!");
 
+// Set the field values from local storage when the page is loaded
 browser.storage.local.get().then(optionsData => {
-    if (optionsData.accentColor === "-moz-win-accentcolor")
+    if (optionsData.accentColor === "-moz-win-accentcolor") {
         document.getElementById("enable-adaptive").checked = true;
-    else document.getElementById("accent-color").value = optionsData.accentColor;
+
+        accentColor.disabled = true;
+        accentColorText.disabled = true;
+    } else {
+        accentColor.value = optionsData.accentColor;
+        accentColorText.value = optionsData.accentColor.toUpperCase();
+    }
 
     document.getElementById("unfocused-theme").value = optionsData.unfocusedTheme;
 });
